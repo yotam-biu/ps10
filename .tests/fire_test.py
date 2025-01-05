0dd96f004dce8a7c146c2ca921ae6ad18c77b4e5
yotam-biu <nagaryo3@biu.ac.il>
2025-01-05 16:57:54 +0200

    Create fire_test.py

---
+++ .tests/fire_test.py
@@ -0,0 +1,18 @@
+import json
+from spread_fire import spred_fire
+
+def test_fire():
+  
+  with open('initial_state.json', 'r') as f:
+      grid = json.load(f)
+  
+  with open('final_state.json', 'r') as f:
+      final_grid = json.load(f)
+  
+  for i in range(100):
+      update_grid = spread_fire(grid)
+      if update_grid == grid:
+          break
+      grid = update_grid
+  
+  assert final_grid == grid
