962e5b17db0a72e5b9dd7db6e4f767c8f21931bb
yotam-biu <nagaryo3@biu.ac.il>
2025-01-05 17:47:03 +0200

    Update fire_test.py

--- .tests/fire_test.py
+++ .tests/fire_test.py
@@ -6,8 +6,8 @@ def test_fire():
   with open('.tests/initial_state.json', 'r') as f:
       grid = json.load(f)
   
-  # with open('.tests/final_state.json', 'r') as f:
-  #     final_grid = json.load(f)
+  with open('.tests/final_state.json', 'r') as f:
+      final_grid = json.load(f)
   
   # for i in range(100):
   #     update_grid = spread_fire(grid)
