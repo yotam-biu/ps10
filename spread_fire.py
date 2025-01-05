afc8e0ced61b54b9b7aae0ff6de6e0bb9abb5b06
yotam-biu <nagaryo3@biu.ac.il>
2025-01-05 17:49:57 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -1,8 +1,11 @@
 def spread_fire(grid):
+
+    grid_size_y = len(grid)
+    grid_size_x = len(grid[0])
     
     update_grid = [[grid[i][j] for j in range(grid_size)] for i in range(grid_size)]
-    for i in range(grid_size-1):      
-        for j in range(grid_size-1):
+    for i in range(grid_size_y-1):      
+        for j in range(grid_size_x-1):
             if grid[i][j] == 1:
                 neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]]
                 if 2 in neighbors:
